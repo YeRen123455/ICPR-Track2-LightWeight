126c04ff00384ba20f15425b06c576457d570286
Boye <34050675+YeRen123455@users.noreply.github.com>
2024-06-17 11:00:12 +0800

    Update complexity.py

--- model/complexity.py
+++ model/complexity.py
@@ -6,7 +6,7 @@ from net import *
 if __name__ == '__main__':
 
     input_img = torch.rand(1,3,512,512).cuda()
-    net = LightweightNetwork().cuda()
+    net = LightWeightNetwork().cuda()
     flops, params = profile(net, inputs=(input_img, ))
     print('Params: %2fM' % (params/1e6))
     print('FLOPs: %2fGFLOPs' % (flops/1e9))
