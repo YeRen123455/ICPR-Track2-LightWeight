d0d906522b75037cf392784e97cf5a270aa7c86f
Boye <34050675+YeRen123455@users.noreply.github.com>
2024-06-20 17:44:25 +0800

    Update Eval.py

--- Eval.py
+++ Eval.py
@@ -5,11 +5,11 @@ Fbase = 12.56         #基线的运算量 GFlops The FLOPs for the baseline
 
 
 # 输入模型测试得到的评价指标 Input evaluation indicators obtained from model testing
-Fa = 1.5e-5 #虚警率 False alarm rate
-Pd = 0.59   #检测率 Detection rate
-IoU = 0.62  #平均交并比 average intersection over union
-Params = 0.9 #模型参数量 M Parameters of your model (Unit: M)
-Flops = 5.08 #模型运算量 GFlops of your model
+Fa = 1.913179e-05 #虚警率 False alarm rate
+Pd = 0.5555   #检测率 Detection rate
+IoU = 0.36217  #平均交并比 average intersection over union
+Params = 0.914057 #模型参数量 M Parameters of your model (Unit: M)
+Flops = 5.1789169 #模型运算量 GFlops of your model
 
 
 #评分 Score
@@ -25,6 +25,6 @@ Se = 1 - ((Params/Pbase + Flops/Fbase) * 0.5)
 # Spe = (Se +Sp)/2
 Spe = (Sp + Se) *0.5
 
-print("IoU: {:.2f}, Fa: {}, Pd: {:.2f}, Sp: {}".format(IoU, Fa, Pd, Sp))
-print("Params: {:.1f}M , FLOPs: {:.2f}GFlops, Se: {}".format(Params, Flops, Se))
+print("IoU: {:.3f}, Fa: {}, Pd: {:.3f}, Sp: {}".format(IoU, Fa, Pd, Sp))
+print("Params: {:.3f}M , FLOPs: {:.3f}GFlops, Se: {}".format(Params, Flops, Se))
 print("Spe: ",format(Spe))
